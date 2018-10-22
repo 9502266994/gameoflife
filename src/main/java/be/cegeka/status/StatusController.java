ec0b373bf3c5e501e68bc9a1b4dc12dd0cbb1198
developer <groeipakket_dev@kindengezin.be>
2018-10-22 09:29:37 +0200

    Frans: prep for workshop UCLL 2018-10-22

--- src/main/java/be/cegeka/status/StatusController.java
+++ src/main/java/be/cegeka/status/StatusController.java
@@ -1,25 +1,16 @@
 package be.cegeka.status;
 
-import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-
-/**
- * Created by fransg on 02/12/15.
- */
 @RestController
 @RequestMapping(value = "/status")
 public class StatusController {
 
     @RequestMapping(value = "/name")
     @ResponseBody
-    public String getApplicationName(){
+    public String getApplicationName() {
         return "{\"name\": \"Game of Life\"}";
     }
 
--- src/test/java/be/cegeka/status/StatusControllerTest.java
+++ src/test/java/be/cegeka/status/StatusControllerTest.java
@@ -4,15 +4,12 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * Created by fransg on 02/12/15.
- */
 public class StatusControllerTest {
 
     private StatusController statusController = new StatusController();
 
     @Test
-    public void testGetApplicationName_ShouldReturnName() throws Exception {
+    public void testGetApplicationName_ShouldReturnName() {
         assertThat(statusController.getApplicationName()).isEqualTo("{\"name\": \"Game of Life\"}");
     }
 }
